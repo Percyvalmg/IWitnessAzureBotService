ec3056db22e658abd570a1bf5d94b799b41a8ccf
Percyval Gonde <percyval.gonde@standardbank.co.za>
2020-10-08 02:54:45 +0200

    Remove unnecessary sending of message

--- bots/iwitnessBot.js
+++ bots/iwitnessBot.js
@@ -4,20 +4,25 @@ class IWitnessBot extends DialogBot {
     constructor(conversationState, userState, dialog) {
         super(conversationState, userState, dialog);
 
-        this.onMessage(async (context, next) => {
-            const reply = {
-                type: 'message',
-                text: 'This is a message with an attachment.',
-                attachments: [
-                    {
-                        contentType: 'image/png',
-                        contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png'
-                    }
-                ]
-            };
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    const reply = {
+                        type: 'message',
+                        text: 'This is a message with an attachment.',
+                        attachments: [
+                            {
+                                contentType: 'image/png',
+                                contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png'
+                            }
+                        ]
+                    };
 
-            await context.sendActivity(reply);
-            await dialog.run(context, conversationState.createProperty('DialogState'));
+                    await context.sendActivity(reply);
+                    await dialog.run(context, conversationState.createProperty('DialogState'));
+                }
+            }
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
